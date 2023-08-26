1eb62be8854c01604237647b9f86e31947748a5f
Vinicius Duarte <duartevini.dev@gmail.com>
2023-08-26 09:23:02 -0300

    first commit

---
+++ app/src/main/java/com/example/tela_de_login/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tela_de_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
